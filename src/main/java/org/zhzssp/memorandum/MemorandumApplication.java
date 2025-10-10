7be3469cb053413df2fa6c1cb9b9bc4ac96ee9ba
H Z <zhzssp666@163.com>
2025-10-10 23:52:08 +0800

    解决csrf问题&美化样式

--- src/main/java/org/zhzssp/memorandum/MemorandumApplication.java
+++ src/main/java/org/zhzssp/memorandum/MemorandumApplication.java
@@ -3,7 +3,7 @@ package org.zhzssp.memorandum;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// 程序主入口，可从浏览器访问http://localhost:8080/register
+// 程序主入口，可从浏览器访问http://localhost:8080
 @SpringBootApplication
 public class MemorandumApplication {
     public static void main(String[] args) {
--- src/main/java/org/zhzssp/memorandum/controller/AuthController.java
+++ src/main/java/org/zhzssp/memorandum/controller/AuthController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
 
 @Controller
 public class AuthController {
@@ -21,7 +22,15 @@ public class AuthController {
     // 不使用"/"时，login/register或addMemo/register等情况会导致无法正确识别表单提交路径（硬编码的缘故）
     // 接收POST /register请求（内容包括user, password）,处理用户注册
     @PostMapping("/register")
-    public String register(@RequestParam String username, @RequestParam String password) {
+    public String register(@RequestParam String username, @RequestParam String password, Model model) {
+        // 后端验证密码长度
+        if (password.length() < 5) {
+            // 密码不符合要求，返回注册页面并显示错误消息
+            model.addAttribute("errorMessage", "Password must be at least 5 characters long.");
+            // 使用redirect会发出GET请求，导致错误信息无法显示
+            return "register";
+        }
+
         User user = new User();
         user.setUsername(username);
         user.setPassword(new BCryptPasswordEncoder().encode(password));
